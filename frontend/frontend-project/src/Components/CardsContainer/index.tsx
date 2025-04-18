f43dd71bf166ca49c506d9bb962c4be3813dfbdd
Nikita <perminovni@gmail.com>
2025-04-18 13:43:08 +0300

    Frontend and backend update

--- frontend/frontend-project/src/Components/CardsContainer/index.tsx
+++ frontend/frontend-project/src/Components/CardsContainer/index.tsx
@@ -1,19 +1,59 @@
 import ItemCard from "../ItemCard";
-import styles from "./CardsContainer.module.css"
+import styles from "./CardsContainer.module.css";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+
+interface Photo {
+    id: number;
+    photo_url: string;
+}
+
+interface Item {
+    id: number;
+    title: string;
+    price: number;
+    available: boolean;
+    preorder: boolean;
+    photos: Photo[];
+}
 
 export default function CardsContainer() {
+    async function fetchData() {
+        const { data } = await axios.get("http://127.0.0.1:8000/api/items/");
+        return data;
+    }
+
+    const { data, isLoading, isError } = useQuery<Item[]>({
+        queryKey: ['products'],
+        queryFn: fetchData,
+    });
+
+    if (isLoading) {
+        return <div>Загрузка...</div>;
+    }
+
+    if (isError) {
+        return <div>Ошибка при загрузке данных</div>;
+    }
+
+    if (!data || data.length === 0) {
+        return <div>Товары не найдены</div>;
+    }
+
     return (
         <div className={styles.MainContainer}>
             <div className={styles.CardsContainer}>
-                <ItemCard/>
-                <ItemCard/>
-                <ItemCard/>
-                <ItemCard/>
-                <ItemCard/>
-                <ItemCard/>
-                <ItemCard/>
-                <ItemCard/>
+                {data.map((item) => (
+                    <ItemCard
+                        key={item.id}
+                        title={item.title}
+                        price={item.price}
+                        available={item.available}
+                        preorder={item.preorder}
+                        photos={item.photos}
+                    />
+                ))}
             </div>
         </div>
-    )
+    );
 }
--- frontend/frontend-project/src/Components/ItemCard/index.tsx
+++ frontend/frontend-project/src/Components/ItemCard/index.tsx
@@ -1,11 +1,33 @@
 import styles from "./ItemCard.module.css"
 
-export default function ItemCard() {
-    return (
-        <div className={styles.MainContainer}>
-            <div className={styles.PhotoContainer}>
 
+interface Photo {
+    id: number;
+    photo_url: string;
+}
+
+interface ItemProps {
+    title: string;
+    price: number;
+    available: boolean;
+    preorder: boolean;
+    photos: Photo[];
+}
+
+export default function ItemCard({title, price, available, preorder, photos}: ItemProps) {
+    return (
+        <a href={''} className={styles.MainContainer}>
+            <div className={styles.PhotoContainer}>
+                <img className={styles.photo} src={`${photos[0].photo_url}`} alt=""/>
             </div>
-        </div>
+            <div className={styles.ContentContainer}>
+                <h1 className={styles.Title}>{title}</h1>
+                <p className={styles.Price}>{price}</p>
+                <p className={`${styles.Preorder} ${!preorder ? styles.hidden : ''}`}>
+                    Предзаказ
+                </p>
+                <p>{available}</p>
+            </div>
+        </a>
     )
 }
